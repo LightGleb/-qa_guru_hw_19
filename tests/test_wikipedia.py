3b403f4146bfa6c02ed4fa8a7f567e82f1e2d501
lightgleb <prorocker7@gmail.com>
2024-04-30 19:34:56 +0300

    1. Настроить систему для работы с устройствами на Android – реальными + эмуляторами (инструкция).
    2. Разработать отдельный тест на getting started (onboarding screen) в приложении википедии - пройти по 4м экранам, на каждом сделать проверку
    
    3. Добавить в config опцию context, по значению которой остальные опции конфига будут загружаться (используя библиотеку python-dotenv) из нужного файла типа .env.context. По итогу должно быть минимум 2 .env-файла, например: .env.local_emulator, .env.bstack

---
+++ tests/test_wikipedia.py
@@ -0,0 +1,60 @@
+from allure import step
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have
+
+
+def test_search():
+    with step('Click "Skip" button'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
+
+    with step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
+
+    with step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Appium'))
+
+
+def test_open_first_article():
+    with step('Click "Skip" button'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
+
+    with step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Hell')
+
+    with step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Hell'))
+
+    with step('Open article'):
+        results.first.click()
+
+
+def test_getting_started():
+    with step('Verify welcome screen'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(have.text('The Free '
+                                                                                                  'Encyclopedia\n…in '
+                                                                                                  'over 300 languages'))
+
+        with step('Click "Continue" button'):
+            browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
+
+        with step('Verify second welcome screen'):
+            browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(have.text('New ways to '
+                                                                                                      'explore'))
+
+        with step('Click "Continue" button'):
+            browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
+
+        with step('Verify third welcome screen'):
+            browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(have.text('Reading lists '
+                                                                                                      'with sync'))
+        with step('Click "Continue" button'):
+            browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
+
+        with step('Verify fourth welcome screen'):
+            browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(have.text('Data & Privacy'))
